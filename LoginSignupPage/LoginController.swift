fe49755d9e16b5f0ecb68c49332545d561b08133
Max Julius Euler <max.euler@mni.thm.de>
2018-12-18 22:02:13 +0100

    Initial Commit

---
+++ LoginSignupPage/LoginController.swift
@@ -0,0 +1,89 @@
+//
+//  ViewController.swift
+//  LoginSignupPage
+//
+//  Created by Max Euler on 18.12.18.
+//  Copyright © 2018 Max Euler. All rights reserved.
+//
+
+import UIKit
+
+class LoginController: UIViewController {
+    
+    let titleLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Login"
+        label.textAlignment = .center
+        return label
+    }()
+    
+    let emailTextField: UITextField = {
+        let textField = UITextField()
+        textField.placeholder = "Email"
+        textField.borderStyle = .roundedRect
+        return textField
+    }()
+
+    let passwordTextField: UITextField = {
+        let textField = UITextField()
+        textField.placeholder = "Password"
+        textField.isSecureTextEntry = true
+        textField.borderStyle = .roundedRect
+        return textField
+    }()
+    
+    let loginButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Login", for: .normal)
+        button.addTarget(self, action: #selector(handleLogin), for: .touchUpInside)
+        return button
+    }()
+    
+    let dontHaveAnAccountButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Register", for: .normal)
+        button.addTarget(self, action: #selector(handleDontHaveAnAccount), for: .touchUpInside)
+        return button
+    }()
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setupViews()
+    }
+    
+    @objc func handleLogin() {
+        
+    }
+    
+    @objc func handleDontHaveAnAccount() {
+        
+    }
+    
+    fileprivate func setupViews() {
+        view.backgroundColor = .white
+        
+        view.addSubview(titleLabel)
+        titleLabel.anchor(top: view.safeAreaLayoutGuide.topAnchor, left: view.leftAnchor, bottom: nil, right: view.rightAnchor, paddingTop: 10, paddingLeft: 20, paddingBottom: 0, paddingRight: 20, width: 0, height: 150)
+        
+        let stackView = UIStackView(arrangedSubviews: [emailTextField, passwordTextField, loginButton])
+        stackView.axis = .vertical
+        stackView.distribution = .fillEqually
+        stackView.spacing = 20
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        
+        view.addSubview(stackView)
+        stackView.heightAnchor.constraint(equalToConstant: 180).isActive = true
+        stackView.widthAnchor.constraint(equalToConstant: view.frame.width * 0.8).isActive = true
+        stackView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        stackView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        
+        view.addSubview(dontHaveAnAccountButton)
+        dontHaveAnAccountButton.anchor(top: nil, left: view.leftAnchor, bottom: view.safeAreaLayoutGuide.bottomAnchor, right: view.rightAnchor, paddingTop: 0, paddingLeft: 20, paddingBottom: 10, paddingRight: 20, width: 0, height: 40)
+        
+    }
+
+
+}
+
